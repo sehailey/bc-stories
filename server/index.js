1082940f2217c1faa8394954316b83a59f39fe8c
notnull <notnull@host.home>
2018-10-16 15:31:25 -0400

    heroku

--- server/index.js
+++ server/index.js
@@ -11,9 +11,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(require('body-parser').text())
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(__dirname, 'build'))
-}
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
 app.use('/api', require('./api'))
 
