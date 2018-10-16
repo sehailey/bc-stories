601db3b798086f53c7d38a00f2acb8074885e473
notnull <notnull@host.home>
2018-10-16 14:42:54 -0400

    added .eslintrc, adjusting heroku-postbuild

--- server/index.js
+++ server/index.js
@@ -8,35 +8,35 @@ app.use(volleyball)
 
 // body parsing middleware
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(require('body-parser').text())
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(__dirname, 'build'))
+  app.use(express.static(__dirname, 'public'))
 }
 
 app.use('/api', require('./api'))
 
 app.get('/hello', (req, res, next) => {
-    try {
-        const data = {data: 'Hello from Express!'}
-        res.send(data)
-    } catch (e) {
-        console.log(e)
-        next(e)
-    }
+  try {
+    const data = { data: 'Hello from Express!' }
+    res.send(data)
+  } catch (e) {
+    console.log(e)
+    next(e)
+  }
 })
 
 // sends index.html
 app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
 })
 
 // error handling endware
 app.use((err, req, res, next) => {
-    console.error(err)
-    console.error(err.stack)
-    res.status(err.status || 500).send(err.message || 'Internal server error.')
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
 })
 
 app.listen(port, () => console.log(`Doin' haxor stuff on port ${port}`))
