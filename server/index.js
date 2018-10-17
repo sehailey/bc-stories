d2315bf109598f7ea928814f5584a9eb92417e94
sehailey <sarah.elizabeth.hailey@gmail.com>
2018-10-17 13:53:05 -0400

    build path

--- server/index.js
+++ server/index.js
@@ -14,10 +14,10 @@ app.use('/api', require('./api'))
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
-  app.use(express.static(path.join(__dirname, 'client', 'build')))
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
 }
 app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
 )
 
 // error handling endware
